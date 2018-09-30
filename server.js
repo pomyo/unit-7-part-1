c29a0c9b38c10ea8a0176161c6ccf38cf3091fd5
pomyo <yeddev@gmail.com>
2018-09-30 02:00:28 -0400

    added useNewUrlParser again

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect("mongodb://heroku_tlwqfxjj:Rocketscience101!@ds113736.mlab.com:13736/heroku_tlwqfxjj");
+mongoose.connect("mongodb://heroku_tlwqfxjj:Rocketscience101!awef@ds113736.mlab.com:13736/heroku_tlwqfxjj", { useNewUrlParser: true }).catch(function (err) {console.log(err)});
 
 
 require('./routes/api-routes.js')(app);
